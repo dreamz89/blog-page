f1c1a78d0b261be088547c45570f905bc75a2825
Jocelyn <jocelyntan89@hotmail.com>
2024-06-19 00:59:32 +0200

    style home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,34 @@
 import { useEffect, useState } from "react"
-import getPosts from "../api/localdata"
-import getPhoto from "../api/unsplash"
-import { PostType } from "../types/post"
+import { Typography } from "antd"
+import styled from "styled-components"
+
+import getPosts from "api/localdata"
+import getPhoto from "api/unsplash"
+import { device } from "styles/breakpoints"
+import { PostType } from "types/post"
+
+const { Title, Paragraph } = Typography
+
+const Page = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 32px;
+  padding: 32px 0;
+`
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 32px;
+
+  @media ${device.sm} {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media ${device.lg} {
+    grid-template-columns: repeat(3, 1fr);
+  }
+`
 
 const Home = () => {
   const [postsData, setPostsData] = useState<PostType[]>()
@@ -26,7 +53,16 @@ const Home = () => {
     getPostsData()
   }, [])
 
-  return <div>Home</div>
+  return (
+    <Page>
+      <div>
+        <Title>Blog</Title>
+        <Paragraph>Follow me on my journey around the world</Paragraph>
+      </div>
+      <Grid>
+      </Grid>
+    </Page>
+  )
 }
 
 export default Home
